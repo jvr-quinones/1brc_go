e359a3d0cd0673c5f9d64a6a5abb9c3fdd728cd9
Javier <jvr.quinones@gmail.com>
2024-07-19 22:58:16 -0500

    Exploring _bufio_ usage

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Print("Welcome to the One Billion Row Challenge")
+	// Welcome to the One Billion Row Challenge in GO
+
+	file := openFile("sample.txt")
+	counter := countLinesV1(file)
+	fmt.Println("Counter V1:", counter)
+
+	counter = countLinesV2(file)
+	fmt.Println("Counter V2:", counter)
+
+}
+
+func countLinesV1(file *os.File) (count int) {
+	file.Seek(0, 0)
+	buffer := bufio.NewReader(file)
+	for line, err := buffer.ReadString('\n'); (len(line) > 0) || err == nil; line, err = buffer.ReadString('\n') {
+		count++
+	}
+	return count
+}
+
+func countLinesV2(file *os.File) (count int) {
+	file.Seek(0, 0)
+	buffer := bufio.NewScanner(file)
+	for buffer.Scan() {
+		count++
+	}
+	return count
+}
+
+func openFile(name string) *os.File {
+	file, err := os.Open(name)
+	if err != nil {
+		log.Fatalf("Error opening %q", name)
+	}
+	return file
 }
